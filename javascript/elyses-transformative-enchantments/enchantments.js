b6e8fa390ff9786fb30b372359485eb72f8a7e3c
U-DESKTOP-GB4LMH2\Stewart <sgray@segludian.com>
2026-01-12 10:13:14 -0700

    100 Days of Code 2026 - Day 12

---
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -0,0 +1,91 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export function seeingDouble(deck) {
+  return deck.map((value) => value * 2)
+}
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export function threeOfEachThree(deck) {
+  // didn't realize they were looking for a reduce call
+  let result =  deck.reduce((new_deck, card) => {
+    if (card == 3)
+      new_deck.push(3, 3, 3)
+    else
+      new_deck.push(card)
+    return new_deck
+  }, []);
+  return result
+}
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export function middleTwo(deck) {
+  return deck.splice(Math.floor(deck.length / 2) -1, 2)
+}
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export function sandwichTrick(deck) {
+  let right = deck.splice(-1, 1)
+  let left = deck.splice(0, 1)
+  deck.splice(Math.floor(deck.length / 2), 0, ...right, ...left)
+  return deck
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+export function twoIsSpecial(deck) {
+  return deck.filter((value) => value == 2)
+}
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+export function perfectlyOrdered(deck) {
+  return deck.sort((item1, item2) => item1 > item2 ? 1 : item1 < item2 ? -1 : 0)
+}
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+export function reorder(deck) {
+  return deck.reverse()
+}
