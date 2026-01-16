bc6b5dd662fe76da5fba26484a475b43d515f114
U-DESKTOP-GB4LMH2\Stewart <sgray@segludian.com>
2026-01-16 10:41:34 -0700

    100DaysOfCodeDay16

---
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -0,0 +1,70 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+export function removeDuplicates(playlist) {
+  return Array.from(new Set(playlist))
+}
+
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+export function hasTrack(playlist, track) {
+  // There is no benefit to changing this into a set.
+  // return playlist.indexOf(track) >= 0
+  // but I did it anyway because that is the learning topic.
+  return new Set(playlist).has(track)
+}
+
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function addTrack(playlist, track) {
+  let current = new Set(playlist)
+  current.add(track)
+  return Array.from(current)  
+}
+
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function deleteTrack(playlist, track) {
+  let current = new Set(playlist)
+  current.delete(track)
+  return Array.from(current)  
+}
+
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+export function listArtists(playlist) {
+  return Array.from(new Set(playlist.map((value, index, array) => {
+    let dash = value.indexOf(' - ')
+    if (dash >= 0)
+      return value.slice(dash + 3)
+    return value
+  })))
+}
