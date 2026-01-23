53cf13b53d991ddb099c7b0a4e5d4f4b06119af7
U-DESKTOP-GB4LMH2\Stewart <sgray@segludian.com>
2026-01-23 10:06:28 -0700

    100 Days of Code 2026 - Day 23

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+/**
+ * Find number associated with color.
+ * @param {String} color 
+ */
+export const colorCode = (color) => 
+  COLORS.indexOf(color.toLowerCase().trim())
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
