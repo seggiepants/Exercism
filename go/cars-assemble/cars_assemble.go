a0f9b8d7a98bd4372926e3492a50f3434bcaf9ab
U-DESKTOP-GB4LMH2\Stewart <sgray@segludian.com>
2026-05-01 15:12:29 -0600

    First 10 learning exercises on go track

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,22 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * (successRate / 100.00)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate)) / 60
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	const singleCar = 10000
+	const tenCars = 95000
+
+	var remainder int = carsCount % 10
+	return uint((remainder * singleCar) + ((carsCount-remainder)/10)*tenCars)
+}
